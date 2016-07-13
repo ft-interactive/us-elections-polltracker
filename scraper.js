07a3cb33a059f4802cef2e11cc47b2d70921e8d2
Joanna Kao <joanna.kao@ft.com>
2016-07-13 16:56:30 -0400

    restyle so closer to eslintable

--- scraper.js
+++ scraper.js
@@ -1,58 +1,57 @@
-var pg = require('pg'),
-	fetch = require('isomorphic-fetch'),
-	winston = require('winston'),
-	db = require('./models/index'),
-	Polldata = require('./models/index').Polldata;
+const fetch = require('isomorphic-fetch');
+const winston = require('winston');
+const db = require('./models/index');
+const Polldata = require('./models/index').Polldata;
 
 // Polldata.sync({force: true}) // use this to drop table and recreate
 db.sequelize.sync();
 
+function addPollAveragesToDatabase(polldate, candidate, value, state) {
+  Polldata.findAll({
+    where: {
+      date: polldate,
+      candidatename: candidate,
+      state: state,
+    },
+  }).then(function(res) {
+    if (res.length > 0) { // already in the db
+      // check to make sure value hasn't changed
+      if (res[0].dataValues.pollaverage !== parseFloat(value)) {
+        Polldata.update({
+          pollaverage: value,
+        }, {
+          where: {
+            id: res[0].dataValues.id,
+          },
+        });
+        winston.log('warn', 'RCP value for '+candidate+' on '+polldate+' changed from '+res[0].dataValues.pollaverage+' to '+value)
+      }
+    } else {
+      Polldata.create({ date: polldate, candidatename: candidate, pollaverage: value, state: state }).then(function(poll) {
+        winston.log('info', 'New poll average added for '+candidate+' on '+polldate+' with value '+value);
+      });
+    }
+  });
+}
+
 function getPollAverageData(rcpURL) {
-	fetch(rcpURL).then(function(response) {
-		response.json().then(function(rcpData) {
-			var datapoints = rcpData.rcp_avg;
-			for (var i = 0; i < datapoints.length; i++) {
-				var datapoint = datapoints[i],
-					polldate = datapoint.date;
-				for (var j = 0; j < datapoint.candidate.length; j++) {
-					candidate = datapoint.candidate[j].name,
-					value = datapoint.candidate[j].value,
-					state = 'us';
+  fetch(rcpURL).then(function(response) {
+    response.json().then(function(rcpData) {
+      const datapoints = rcpData.rcp_avg;
+      for (let i = 0; i < datapoints.length; i++) {
+        const datapoint = datapoints[i];
+        const polldate = datapoint.date;
+        for (let j = 0; j < datapoint.candidate.length; j++) {
+          const candidate = datapoint.candidate[j].name;
+          const value = datapoint.candidate[j].value;
+          const state = 'us';
 
-					addPollAveragesToDatabase(polldate, candidate, value, state);
-				}
-			}
-		})
-	})
+          addPollAveragesToDatabase(polldate, candidate, value, state);
+        }
+      }
+    });
+  });
 }
 
-function addPollAveragesToDatabase(polldate, candidate, value, state) {	
-	Polldata.findAll({
-		where: {
-			date: polldate,
-			candidatename: candidate,
-			state: state
-		}
-	}).then(function(res) {
-		if (res.length > 0) { // already in the db
-			// check to make sure value hasn't changed
-			if (res[0].dataValues.pollaverage != parseFloat(value)) {
-				Polldata.update({
-					pollaverage: value
-				}, {									
-					where: {
-						id: res[0].dataValues.id
-					}
-				})
-				winston.log('warn', 'RCP value for '+candidate+' on '+polldate+' changed from '+res[0].dataValues.pollaverage+' to '+value)
-			}
-		} else {
-			Polldata.create({ date: polldate, candidatename: candidate, pollaverage: value, state: state }).then(function(poll) {
-				winston.log('info', 'New poll average added for '+candidate+' on '+polldate+' with value '+value)
-			})
-		}
-	})
-}
-
-const raceId = "5491";
+const raceId = '5491';
 getPollAverageData(`http://www.realclearpolitics.com/poll/race/${raceId}/historical_data.json`);
