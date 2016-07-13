8231864d64f050934e39e090333d2424e3cff7c7
Joanna Kao <joanna.kao@ft.com>
2016-07-13 15:47:45 -0400

    dynamically set domain for y axis. start fill between lines

--- layouts/drawChart.js
+++ layouts/drawChart.js
@@ -1,5 +1,6 @@
 const d3 = require('d3');
 const getJSDomWindow = require('./getJSDomWindow');
+const _ = require('underscore');
 
 function round_1dp(x) {
   return Math.round(x * 10) / 10;
@@ -14,15 +15,38 @@ async function drawChart(width, height, fontless, background, logo, startDate, e
 
   const graphWidth = width;
   const graphHeight = height;
-  const margins = { top: 55, bottom: 50, left: 20, right: 30 };
+  const margins = { top: 70, bottom: 50, left: 30, right: 30 };
   const userInputParse = d3.timeParse('%B %e, %Y');
   const colors = { Clinton: '#238fce', Trump: '#e5262d' };
 
+  const data_groupedBy_candidate = _.groupBy(data, function(row) { return row.candidatename; });
+  const data_groupedBy_date = _.groupBy(data, function(row) { return row.date; });
+
   // need more margin right if end date is too close to last datapoint
-  // console.log('dates', ((new Date(userInputParse(endDate)) - new Date(data.Clinton[data.Clinton.length - 1].date)) / 86400000));
-  if (((new Date(userInputParse(endDate)) - new Date(data.Clinton[data.Clinton.length - 1].date)) / 86400000) < 60) {
-    margins.right = 90 - ((new Date(userInputParse(endDate)) - new Date(data.Clinton[data.Clinton.length - 1].date)) / 86400000);
+  if (((new Date(userInputParse(endDate)) - new Date(d3.keys(data_groupedBy_date)[d3.keys(data_groupedBy_date).length - 1])) / 86400000) < 60) {
+    margins.right = 90 - ((new Date(userInputParse(endDate)) - new Date(d3.keys(data_groupedBy_date)[d3.keys(data_groupedBy_date).length - 1])) / 86400000);
   }
+
+  // format for d3.stack
+  const formattedData = [];
+  Object.keys(data_groupedBy_date).forEach(function(date) {
+    let clintonVal;
+    let trumpVal;
+
+    if (data_groupedBy_date[date][0].candidatename === 'Clinton') {
+      clintonVal = data_groupedBy_date[date][0].pollaverage;
+      trumpVal = data_groupedBy_date[date][1].pollaverage;
+    } else {
+      clintonVal = data_groupedBy_date[date][1].pollaverage;
+      trumpVal = data_groupedBy_date[date][0].pollaverage;
+    }
+
+    formattedData.push({
+      date: date,
+      Clinton: clintonVal,
+      Trump: trumpVal,
+    });
+  });
 
   const svg = d3.select(el)
     .append('svg')
@@ -30,15 +54,18 @@ async function drawChart(width, height, fontless, background, logo, startDate, e
     .attr('width', graphWidth)
     .attr('height', graphHeight);
 
+  const [min, max] = d3.extent(data, (d) => d.pollaverage);
+  const yScalePadding = (max - min) / 4;
+
   const yScale = d3.scaleLinear()
-    .domain([30, 55])
+    .domain([min - yScalePadding, max + yScalePadding])
     .range([graphHeight - margins.top - margins.bottom, 0]);
 
   const yAxis = d3.axisLeft()
     .scale(yScale)
     .tickSizeInner(graphWidth - margins.left - margins.right)
     .tickSizeOuter(0)
-    .ticks(3)
+    .ticks(7)
     .tickPadding(-margins.left);
 
   const yLabel = svg.append('g')
@@ -50,15 +77,8 @@ async function drawChart(width, height, fontless, background, logo, startDate, e
 
   yLabel.selectAll('text')
       .attr('transform', function() {
-          return 'translate(' + (-margins.left / 2) + ',-10)';
+          return 'translate(' + (-margins.left - 7) + ',-10)';
       });
-
-  // const yAxisLabel = yLabel.append('text')
-  //   .text('%')
-  //   .style('text-anchor', 'start')
-  //   .attr('class', 'axisLabel')
-  //   .attr('x', -graphWidth + margins.right + margins.left)
-  //   .attr('y', 0);
 
   const xScale = d3.scaleTime()
     .domain([userInputParse(startDate), userInputParse(endDate)])
@@ -69,7 +89,7 @@ async function drawChart(width, height, fontless, background, logo, startDate, e
   const xAxis = d3.axisBottom()
     .scale(xScale)
     .tickValues(xAxisTicks)
-    .tickFormat(function(d, i) {
+    .tickFormat(function(d) {
       return d3.timeFormat('%b %e, %Y')(d);
     });
 
@@ -88,8 +108,40 @@ async function drawChart(width, height, fontless, background, logo, startDate, e
       return 'end';
     });
 
-  const candidateGroups = svg.selectAll('g.candidate')
-    .data(d3.keys(data))
+  if (type === 'area') {
+    const candidateAreas = svg.append('g')
+      .datum(formattedData)
+      .attr('transform', function() {
+        return 'translate('+(margins.left)+','+(margins.top)+')'
+      });
+
+    const area = d3.area()
+      .x(function(d) { return round_1dp(xScale(new Date(d.date))); })
+      .y1(function(d) { return round_1dp(yScale(d.Clinton)); });
+
+    candidateAreas.append('clipPath')
+      .attr('id', 'clip-below')
+      .append('path')
+      .attr('d', area.y0(graphHeight));
+
+    candidateAreas.append('clipPath')
+      .attr('id', 'clip-above')
+      .append('path')
+      .attr('d', area.y0(0));
+
+    candidateAreas.append('path')
+      .attr('class', 'area above')
+      .attr('clip-path', 'url(#clip-above)')
+      .attr('d', area.y0(function(d) { return round_1dp(yScale(d.Trump)); }));
+
+    candidateAreas.append('path')
+      .attr('class', 'area below')
+      .attr('clip-path', 'url(#clip-below)')
+      .attr('d', area);
+  }
+
+  const candidateGroups = svg.selectAll('g.candidateLine')
+    .data(d3.keys(data_groupedBy_candidate))
     .enter()
     .append('g')
     .attr('class', function(d) { return 'candidate ' + d; })
@@ -98,14 +150,14 @@ async function drawChart(width, height, fontless, background, logo, startDate, e
     });
 
   const convertLineData = d3.line()
-    .x(function(d) { return round_1dp(xScale(d.date)); })
-    .y(function(d) { return round_1dp(yScale(d.pollaverage)); });
+      .x(function(d) { return round_1dp(xScale(d.date)); })
+      .y(function(d) { return round_1dp(yScale(d.pollaverage)); });
 
   const candidateLine = candidateGroups.append('path')
     .attr('class', 'candidateLine')
-    .attr('d', function(d) { return convertLineData(data[d]); })
-      .style('stroke', function(d) { return colors[d]; })
-      .style('stroke-width', '2');
+    .attr('d', function(d) { return convertLineData(data_groupedBy_candidate[d]); })
+    .style('stroke', function(d) { return colors[d]; })
+    .style('stroke-width', '2');
 
   const annotationGroup = svg.append('g')
     .attr('class', 'annotations')
@@ -114,36 +166,36 @@ async function drawChart(width, height, fontless, background, logo, startDate, e
     });
 
   const lastPointLabels = annotationGroup.selectAll('circle.lastpointlabel')
-    .data(d3.keys(data))
+    .data(d3.keys(data_groupedBy_candidate))
     .enter()
     .append('circle')
     .attr('class', 'lastpointlabel')
     .attr('cx', function(d) {
-      return round_1dp(xScale(data[d][data[d].length - 1].date));
+      return round_1dp(xScale(data_groupedBy_candidate[d][data_groupedBy_candidate[d].length - 1].date));
     })
     .attr('cy', function(d) {
-      return round_1dp(yScale(data[d][data[d].length - 1].pollaverage));
+      return round_1dp(yScale(data_groupedBy_candidate[d][data_groupedBy_candidate[d].length - 1].pollaverage));
     })
     .attr('r', '5')
     .style('fill', function(d) { return colors[d]; });
 
   const lastPointText = annotationGroup.selectAll('text.lastpointtext')
-    .data(d3.keys(data))
+    .data(d3.keys(data_groupedBy_candidate))
     .enter()
     .append('text')
     .attr('class', 'lastpointtext')
-    .text(function(d) { return data[d][data[d].length - 1].pollaverage + ' ' + d; })
+    .text(function(d) { return data_groupedBy_candidate[d][data_groupedBy_candidate[d].length - 1].pollaverage + ' ' + d; })
     .attr('x', function(d) {
-      return round_1dp(xScale(data[d][data[d].length - 1].date)) + 10;
+      return round_1dp(xScale(data_groupedBy_candidate[d][data_groupedBy_candidate[d].length - 1].date)) + 10;
     })
     .attr('y', function(d) {
       let onTop; // adjust for when labels overlap
       let yOverlapOffset;
 
-      if (data.Clinton[data.Clinton.length - 1].pollaverage > data.Trump[data.Trump.length - 1].pollaverage) {
+      if (data_groupedBy_candidate.Clinton[data_groupedBy_candidate.Clinton.length - 1].pollaverage > data_groupedBy_candidate.Trump[data_groupedBy_candidate.Trump.length - 1].pollaverage) {
         onTop = 'Clinton';
       }
-      if (data.Clinton[data.Clinton.length - 1].pollaverage < data.Trump[data.Trump.length - 1].pollaverage) {
+      if (data_groupedBy_candidate.Clinton[data_groupedBy_candidate.Clinton.length - 1].pollaverage < data_groupedBy_candidate.Trump[data_groupedBy_candidate.Trump.length - 1].pollaverage) {
         onTop = 'Trump';
       }
 
@@ -153,7 +205,7 @@ async function drawChart(width, height, fontless, background, logo, startDate, e
         yOverlapOffset = -5;
       }
 
-      return round_1dp(yScale(data[d][data[d].length - 1].pollaverage) - yOverlapOffset);
+      return round_1dp(yScale(data_groupedBy_candidate[d][data_groupedBy_candidate[d].length - 1].pollaverage) - yOverlapOffset);
     })
     .style('fill', function(d) { return colors[d]; });
 
@@ -165,19 +217,19 @@ async function drawChart(width, height, fontless, background, logo, startDate, e
       return 'Which White House candidate is leading in the polls?';
     })
     .attr('class', 'headline')
-    .attr('x', -margins.left / 2)
+    .attr('x', -margins.left + 7)
     .attr('y', -margins.top + 24);
 
   const subhead = annotationGroup.append('text')
-    .text('National polling average as of ' + d3.timeFormat('%B %e, %Y')(data.Clinton[data.Clinton.length - 1].date) + ' (%)')
+    .text('National polling average as of ' + d3.timeFormat('%B %e, %Y')(new Date(formattedData[formattedData.length - 1].date)) + ' (%)')
     .attr('class', 'subhead')
-    .attr('x', -margins.left / 2)
+    .attr('x', -margins.left + 7)
     .attr('y', -margins.top + 46);
 
   const sourceline = annotationGroup.append('text')
     .text('Source: Real Clear Politics')
     .attr('class', 'sourceline')
-    .attr('x', -margins.left / 2)
+    .attr('x', -margins.left + 7)
     .attr('y', graphHeight - margins.top - 10)
     .style('text-anchor', 'start');
 
