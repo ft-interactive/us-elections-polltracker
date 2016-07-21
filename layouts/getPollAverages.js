ecc01bcb3e30ca103004beae29de01b5519fc401
Joanna Kao <joanna.kao@ft.com>
2016-07-21 13:26:56 -0400

    delete timezone offset when querying for pollaverages

--- layouts/getPollAverages.js
+++ layouts/getPollAverages.js
@@ -1,5 +1,9 @@
 var	db = require('../models/index'),
+	_ = require('underscore'),
+	d3 = require('d3'),
 	Pollaverages = require('../models/index').Pollaverages;
+
+const deleteTimezoneOffset = d3.timeFormat('%B %e, %Y');
 
 // runs a psql query to get data from db
 async function getPollAverages(state, startDate, endDate) {
@@ -15,6 +19,12 @@ async function getPollAverages(state, startDate, endDate) {
 			['date', 'ASC']
 		],
 		raw: true
+	})
+	.then(function(data) {
+		data = _.each(data, function(row) {
+			row.date = new Date(deleteTimezoneOffset(row.date));
+		});
+		return data;
 	});
 }
 
