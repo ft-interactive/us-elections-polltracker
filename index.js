0c90834fa635f67eb6f9172af4fcd0a6c65c13cc
Joanna Kao <joanna.kao@ft.com>
2016-07-21 17:32:57 -0400

    move commas filter to filter.js file

--- index.js
+++ index.js
@@ -55,9 +55,6 @@ function convertToCacheKeyName(queryRequest) {
 const env = nunjucks.configure('views', {
   autoescape: true,
   express: app,
-})
-.addFilter('commas', function (n) {
-    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 });
 
 Object.assign(env.filters, filters);
