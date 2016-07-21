58df07c2af8e378d47ff8c65778aaa800b8e3712
Joanna Kao <joanna.kao@ft.com>
2016-07-21 18:15:48 -0400

    add doctype to html stub not seeing a visual difference, but more correct

--- layouts/drawChart.js
+++ layouts/drawChart.js
@@ -7,7 +7,7 @@ function round_1dp(x) {
   return Math.round(x * 10) / 10;
 }
 async function drawChart(options, data) {
-  const htmlStub = '<html><head></head><body><div id="dataviz-container"></div><script src="https://d3js.org/d3.v4.min.js"></script></body></html>';
+  const htmlStub = '<!doctype html><head></head><body><div id="dataviz-container"></div><script src="https://d3js.org/d3.v4.min.js"></script></body></html>';
 
   const window = await getJSDomWindow(htmlStub);
   const el = window.document.querySelector('#dataviz-container');
