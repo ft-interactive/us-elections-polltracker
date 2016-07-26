6602daaae724d653f5a0788ed5e7327fd62128c6
Luke Kavanagh <kavanagh@users.noreply.github.com>
2016-07-26 16:36:56 +0100

    Analytics: add o-tracking, set up new GA property (#56)
    * Analytics: add o-tracking, set up new GA property
    
    Sends Analytics to Spoor and Lantern.
    
    Includes a “microsite_name” field of “us-elections” which allows for
    easier custom queries on the Spoor platform.
    
    This also changes the Google Analytics UA code to use a Property set up
    specifically for this microsite and includes link tracking and
    demographic data.
    
    The client side tracking JS is temporary. It works well enough but I’m
    going to refactor client JS loading so that it’s asynchronous, once
    I’ve done that I’ll tidy this code up.
    
    * Mistakenly set analytics flags to "On"

--- config/flags.js
+++ config/flags.js
@@ -1,11 +1,11 @@
 const prod = process.env.NODE_ENV === 'production';
 
-export default _ => ({
-  prod: prod,
+export default () => ({
+  prod,
   errorReporting: prod,
   analytics: prod,
   googleAnalytics: prod,
   ads: true,
   shareButtons: true,
-  onwardjourney: false
-})
+  onwardjourney: false,
+});
