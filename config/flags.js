dc6137aa9267d2a2826b880f642e43dc426e11e6
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-07-26 12:33:14 +0100

    Mistakenly set analytics flags to "On"

--- config/flags.js
+++ config/flags.js
@@ -3,8 +3,8 @@ const prod = process.env.NODE_ENV === 'production';
 export default () => ({
   prod,
   errorReporting: prod,
-  analytics: true,
-  googleAnalytics: true,
+  analytics: prod,
+  googleAnalytics: prod,
   ads: true,
   shareButtons: true,
   onwardjourney: false,
