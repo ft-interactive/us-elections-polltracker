4189ccb70f9310bf2d8b16e13bf06bc74833be42
tomgp <tom.g.pearson@gmail.com>
2016-08-03 17:54:26 +0100

    add path generator stuff

--- layouts/timeseries-layout.js
+++ layouts/timeseries-layout.js
@@ -1,8 +1,29 @@
 const d3 = require('d3');
 const timeFormat = d3.timeFormat('%B %e, %Y');
+const timeParse = d3.timeParse('%B %e, %Y');
+const oneDP = (d) => (+d3.format('.1f'));
+const candidates = ['Trump', 'Clinton'];
+const candidateColor = {
+  Trump: {
+    line: '#e5262d',
+    area: '#f4a098',
+  },
+  Clinton: {
+    line: '#238fce',
+    area: '#a2c1e1',
+  },
+};
 
+function roundExtent(ext, divisor) {
+  return [(ext[0] - ext[0] % divisor), (ext[1] + (divisor - ext[1] % divisor))];
+}
+
+function round1dp(x) {
+  return Math.round(x * 10) / 10;
+}
 
 function timeseriesLayout(data, opts) {
+  console.log(data);
   const [svgWidth, svgHeight] = (opts.size || '600x300').split('x');
   const layout = {};
   Object.assign(layout, {
@@ -16,9 +37,9 @@ function timeseriesLayout(data, opts) {
     type: opts.type || 'area',
     state: opts.state || 'us',
     logo: (opts.logo ? opts.logo === 'true' : false),
-    title: 'Which White House candidate is leading in the polls?',
-    subtitle: '!National polling average as of August 2, 2016 (%)',
-    source: '!Source: Real Clear Politics',
+    title: '!!!Which White House candidate is leading in the polls?',
+    subtitle: 'National polling average as of August 2, 2016 (%)',
+    source: 'Source: Real Clear Politics',
     yLabelOffset: '-7',
     margin: {
       top: 70,
@@ -29,11 +50,48 @@ function timeseriesLayout(data, opts) {
   });
 
   // make the scales
+  let rawExtent = d3.extent(data, (d) => d.pollaverage);
+  let tickInterval = 5;
+  if (rawExtent[1] - rawExtent[0] < 5) {
+    tickInterval = 1;
+  }
+  rawExtent = [rawExtent[0] - 1, rawExtent[1] + 1];
+  const extent = roundExtent(rawExtent, tickInterval);
 
-  layout.yTicks = [{ label: '', position: '' }];
+  const yScale = d3.scaleLinear()
+    .domain(extent)
+    .range([layout.height - layout.margin.top - layout.margin.bottom, 0]);
+
+  let tickCount = (extent[1] - extent[0]) / tickInterval;
+  if (tickCount < 3) {
+    tickCount = Math.round(extent[1] - extent[0]);
+  }
+
+  const xScale = d3.scaleTime()
+    .domain([timeParse(layout.startDate), timeParse(layout.endDate)])
+    .range([0, layout.width - (layout.margin.left + layout.margin.right)]);
+
+  // make the path generators etc.
+  const path = d3.line()
+      .x(d => round1dp(xScale(d.date)))
+      .y(d => round1dp(yScale(d.pollaverage)));
+
+  layout.yTicks = yScale.ticks(tickCount).map(d => ({
+    label: d,
+    position: yScale(d),
+  })); // [{ label: '', position: '' }];
+
   layout.xTicks = [{ label: '', position: '' }];
+
   layout.candidateAreas = [];
-  layout.candidateLines = [];
+
+  layout.candidateLines = candidates.map(function (d) {
+    return {
+      stroke: candidateColor[d].line,
+      path: path(data.filter((row) => (row.candidatename === d))),
+    };
+  });
+
   layout.candidateEndPoints = [];
 
   console.log(layout);
