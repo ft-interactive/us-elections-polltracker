088ac0cb11441bef7f9a9df33d491cebf949d242
Joanna Kao <joanna.kao@ft.com>
2016-08-30 15:14:27 -0400

    to capture data from anytime during day, set endDate to start of next day

--- layouts/getPollAverages.js
+++ layouts/getPollAverages.js
@@ -1,12 +1,16 @@
 var	db = require('../models/index'),
 	_ = require('underscore'),
 	d3 = require('d3'),
+	moment = require('moment'),
 	Pollaverages = require('../models/index').Pollaverages;
 
 const deleteTimezoneOffset = d3.timeFormat('%B %e, %Y');
 
 // runs a psql query to get data from db
 async function getPollAverages(state, startDate, endDate) {
+	// to capture data from anytime during the day (and timezone offsets), set endDate
+	// to the start of the next day
+	endDate = moment(endDate).add(1, 'day').format();
 	return Pollaverages.findAll({
 		where: {
 			state: state,
