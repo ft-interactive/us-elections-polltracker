b97776d32d73794b61985b494cdfe23703a8d457
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-09-04 14:10:36 +0100

    Redirect old US page

--- server/controllers/state-code-redirect.js
+++ server/controllers/state-code-redirect.js
@@ -1,6 +1,11 @@
 import { codeToSlug } from '../pages/state-page';
 
 export default async (req, res) => {
+  if (req.params.code === 'us') {
+    res.redirect(301, '/polls');
+    return;
+  }
+
   const slug = codeToSlug(req.params.code);
 
   if (!slug) {
