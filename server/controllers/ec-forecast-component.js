40e37925a3b7f5c45974e1b0348aac387eda1009
tomgp <tom.g.pearson@gmail.com>
2016-09-08 10:47:11 +0100

    more cache key tweaking

--- server/controllers/ec-forecast-component.js
+++ server/controllers/ec-forecast-component.js
@@ -16,7 +16,7 @@ export default async (req, res, type) => {
     layout.fontless = true;
   }
   const html = await cache(
-    'ec-forecast-component-fontless:'+layout.fontless,
+    'ec-forecast-component-fontless:' + layout.fontless + layout.ancestorSelector,
     async () => render('ec-forecast-component.html', layout)
   );
   if (type === 'json'){
