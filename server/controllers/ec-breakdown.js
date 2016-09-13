e8a7f1718fe08b60156e3f3c8946d05bcd5c5e31
tomgp <tom.g.pearson@gmail.com>
2016-09-13 15:08:49 +0100

    basic route for ec breakdown graphic

---
+++ server/controllers/ec-breakdown.js
@@ -0,0 +1,20 @@
+import cache from '../lib/cache';
+import { render } from '../nunjucks';
+import nationalCount from '../lib/national-count';
+import ecForecastBarsLayout from '../../layouts/ec-forecastbars-layout';
+
+const maxAge = 120;
+const sMaxAge = 10;
+const cacheControl = `public, max-age=${maxAge}, s-maxage=${sMaxAge}`;
+
+export default async (req, res, type) => {
+  res.setHeader('Cache-Control', cacheControl);
+  const count = await nationalCount();
+  const layout = { title: 'breakdown', fontless:false, };
+  const svg = await cache(
+    'ec-breakdown-fontless:' + layout.fontless,
+    async () => render('ec-breakdown.svg', layout)
+  );
+
+  res.send(svg);
+};
