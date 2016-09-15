c86eecae75b65378d21dd1c3405764d6ace77229
tomgp <tom.g.pearson@gmail.com>
2016-09-15 19:45:37 +0100

    adding grid and bars to template

--- layouts/ec-breakdown-layout.js
+++ layouts/ec-breakdown-layout.js
@@ -1,14 +1,40 @@
 import classifyState from './state-classifications';
 
+function splitArray(a, keyFunction) {
+    const o = {};
+    if (!keyFunction) keyFunction = function (d) { return String(d); };
+    a.forEach(function (d) {
+        const key = keyFunction(d);
+        if (!o[key]) o[key] = [];
+        o[key].push(d);
+    });
+    return o;
+}
+
+function combineMENE(lookup){
+    //get a list a 
+    return lookup;
+}
+
+function percentOfCA(votes){ // electoral votes as a proportion of california i.e. california bar is 100% of available width  
+    return votes/55 * 100;
+}
+
 export default function (stateLookup) {
     // classify the data and sort by electoral college votes
-  const states = Object.keys(stateLookup)
+  const states = Object.keys(combineMENE(stateLookup))
         .map(function (key) {
           const state = stateLookup[key];
           const forecast = (state.code === 'ME' || state.code === 'ME')
             ? classifyState.forecastMENE(state.margin) : classifyState.forecast(state.margin);
 
-          return Object.assign({ forecast }, stateLookup[key]);
+          return { 
+                forecast,
+                name:stateLookup[key].name,
+                code:stateLookup[key].code,
+                ecVotes:stateLookup[key].ecVotes,
+                barPct:percentOfCA(stateLookup[key].ecVotes),
+            };
         })
         .sort((a, b) => b.ecVotes - a.ecVotes);
 
@@ -16,20 +42,9 @@ export default function (stateLookup) {
 
   return {
     title: 'Electoral college breakdown',
-    fontless: false,
+    standalone: true,
+    fontless: true,
     order: classifyState.forecast.range(),
     stateGroups,
   };
-}
-
-
-function splitArray(a, keyFunction) {
-  const o = {};
-  if (!keyFunction) keyFunction = function (d) { return String(d); };
-  a.forEach(function (d) {
-    const key = keyFunction(d);
-    if (!o[key]) o[key] = [];
-    o[key].push(d);
-  });
-  return o;
 }
