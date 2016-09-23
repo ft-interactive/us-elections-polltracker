012f81d63c8f832ffd944065a2be725ab0ebf9f0
Callum Locke <callum.locke@gmail.com>
2016-09-23 15:20:26 +0100

    remove makefile experiment; revert to npm scripts

--- layouts/getLastUpdated.js
+++ layouts/getLastUpdated.js
@@ -2,5 +2,5 @@ import db from '../models';
 
 // runs a psql query to get data from db
 export default () =>
-  db.lastupdates.findOne({ raw: true }).then(data => data.lastupdate)
+  db.lastupdates.findOne({ raw: true }).then(data => data && data.lastupdate)
 ;
