ad0adabf00dbe90681f9087e88b62ab8cff69ee4
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-09-28 10:14:01 +0100

    Refactor cache to refresh in the background and serve stale

--- server/lib/cache.js
+++ server/lib/cache.js
@@ -1,24 +1,79 @@
 import lruCache from 'lru-cache';
+import debugLogger from 'debug';
 
-const maxAge = 60 * 1000; // 60 seconds
+const debug = debugLogger('cache');
+const pending = new Map();
+const maxAge = 60 * 1000; // 60 secs
 
-export const lru = lruCache({
+const lru = lruCache({
   maxAge,
-  max: 500,
+  max: 200000, // 200K (ish)
+  stale: true,
+  length: (value, key) =>
+      (!value ? 0 : JSON.stringify(value).length) + JSON.stringify(key).length
 });
 
-const noop = () => {};
-
-export default async function cache(keyFn = noop, refresh = noop, max = maxAge) {
+export default function cache(keyFn, job, max = maxAge) {
   const key = typeof keyFn === 'string' ? keyFn : keyFn();
 
-  if (!key) return await refresh();
+  // key can be falsey to bypass caching. e.g.
+  // const keyFn = () => {
+  //    return !bypassCache && 'my-key';
+  // }
+  if (!key) return job();
 
-  if (lru.has(key)) {
-    return lru.get(key);
+  if (pending.has(key)) {
+   debug('already pending %s', key);
+   return pending.get(key);
   }
 
-  const value = await refresh();
-  lru.set(key, value, max);
-  return value;
+  // grab the current value in case the key is
+  // just about to be deleted so we can serve
+  // stale in the event of a refresh error
+  let stale = lru.peek(key);
+
+  const isCached = lru.has(key);
+  const hasStaleValue = !!stale;
+
+  function refresh() {
+    debug('refreshing %s', key);
+    return job()
+      .then(result => {
+        debug('caching value %s', key);
+        lru.set(key, result, max);
+        pending.delete(key);
+        return result;
+      })
+      .catch(err => {
+        pending.delete(key);
+
+        if (!hasStaleValue) {
+          throw err;
+        }
+
+        // swallow the error and serve stale
+        debug('restore previous value %s', key)
+        lru.set(key, stale, max);
+        return stale;
+      });
+  }
+
+  if (isCached) {
+    debug('cache hit %s', key);
+    return Promise.resolve(lru.get(key));
+  } else if (hasStaleValue) {
+    debug('start BG refresh and return stale value %s', key);
+    const p = Promise.resolve(stale);
+    pending.set(key, p);
+    process.nextTick(() => {
+      refresh().catch(err => {
+        console.error('Failed background refresh', err.message);
+      });
+    });
+    return p;
+  }
+
+  const pendingPromise = refresh();
+  pending.set(key, pendingPromise);
+  return pendingPromise;
 }
