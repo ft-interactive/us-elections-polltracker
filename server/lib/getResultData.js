a28aea4d4a0130066244579384334dfff695a89d
tomgp <tom.g.pearson@gmail.com>
2016-10-11 19:05:56 +0100

    create selectors

--- server/lib/getResultData.js
+++ server/lib/getResultData.js
@@ -8,14 +8,22 @@ export default async function getResult() {
             const processed = {};
             const totals = response.data.electoralCollege
                 .reduce((previous, current) => {
-                        return {
-                            rep: previous.rep + Number(current.rep),
-                            dem: previous.dem + Number(current.dem),
-                            ind: previous.ind + Number(current.ind),
-                            total: previous.total,
-                        };
-                    }, { rep: 0, dem: 0, ind: 0, total: 538 });
+                    console.log(current);
+                        let r = previous.r;
+                        let d = previous.d;
+                        let g = previous.g;
+                        let l = previous.l;
 
+                        if(current.winner == 'r'){ r += current.ecvotes; }
+                        if(current.winner == 'd'){ d += current.ecvotes; }
+                        if(current.winner == 'g'){ g += current.ecvotes; }
+                        if(current.winner == 'l'){ l += current.ecvotes; }
+                        return { r, d, g, l, total:previous.total};
+                    }, { r: 0, d: 0, g: 0, l:0, total: 538 });
+
+                console.log(totals);
+
+            processed.pollClosingTimes = response.data.events;
             processed.senate = response.data.senate[0];
             processed.house = response.data.house[0];
             processed.electoralCollege = response.data.electoralCollege;
