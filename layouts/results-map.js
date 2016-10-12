113a0ae017411e77848d82f2bf30c5a1a27e0894
tomgp <tom.g.pearson@gmail.com>
2016-10-12 18:01:46 +0100

    create standard map layout

---
+++ layouts/results-map.js
@@ -0,0 +1,32 @@
+import color from './color';
+
+// this module produces 3 css statements, one for each party and one for undecided
+
+export default (electoralCollegeData, opts) => {
+    const selectorGroups = makeSelectorStatements(electoralCollegeData); 
+    return selectorGroups;
+};
+
+function makeSelectorStatements(arr){
+    const groups = arr.reduce(function(previous, current){
+        if(current.winner){
+            previous[current.winner].push( '#' + current.code );
+        }else{
+            previous.n.push( '#' + current.code );
+        }
+        return previous;
+    },
+    {
+        r: [],
+        d: [],
+        g: [],
+        l: [],
+        n: [],
+    });
+
+    Object.keys(groups).forEach(function(key){
+        groups[key] = groups[key].join(', ');
+    })
+
+    return groups;
+}
--- server/lib/getResultData.js
+++ server/lib/getResultData.js
@@ -12,20 +12,71 @@ export default async function getResult() {
                         let d = previous.d;
                         let g = previous.g;
                         let l = previous.l;
+                        let reported = previous.reported;
 
                         if(current.winner == 'r'){ r += current.ecvotes; }
                         if(current.winner == 'd'){ d += current.ecvotes; }
                         if(current.winner == 'g'){ g += current.ecvotes; }
                         if(current.winner == 'l'){ l += current.ecvotes; }
-                        return { r, d, g, l, total:previous.total};
-                    }, { r: 0, d: 0, g: 0, l:0, total: 538 });
+                        if(current.winner){ reported[current.code.substring(0,2)] = true; }
+
+                        return { r, d, g, l, total:previous.total, reported:reported};
+                    }, { r: 0, d: 0, g: 0, l:0, total: 538, reported:{} });
 
             processed.pollClosingTimes = response.data.events;
             processed.senate = response.data.senate[0];
             processed.house = response.data.house[0];
             processed.electoralCollege = response.data.electoralCollege;
             processed.ecTotals = totals;
+            processed.overview = {
+                time: new Date(),
+                senate: response.data.senate[0].current,
+                house: response.data.house[0].current,
+                president: {
+                    clinton: totals.d,
+                    trump: totals.r,
+                    clinton_pct: 100/totals.total * totals.d,
+                    trump_pct: 100/totals.total * totals.r,
+                    /*
+                    bestGuessClinton: 245,
+                    bestGuessTrump: 212,
+                    bestGuessClinton_pct: 45.5,
+                    bestGuessTrump_pct: 39.4, */
+                    states_reporting:Object.keys(totals.reported).length,
+                    isFinal: false
+                },
+            }
 
             return processed;
         });
 }
+
+/*overview
+
+            'president': {
+                'clinton': 90,
+                'trump': 61,
+                'clinton_pct': 1,
+                'trump_pct': 1,
+                'bestGuessClinton': 245,
+                'bestGuessTrump': 212,
+                'bestGuessClinton_pct': 45.5,
+                'bestGuessTrump_pct': 39.4,
+                'footnote': '(12 of 50 states reporting as of 9:28am)',
+                'isFinal': false
+            },
+            'senate': {
+                'dem': 35,
+                'rep': 25,
+                'ind': 1,
+                'dem_contested': 9,
+                'rep_contested': 20
+            },
+            'house': {
+                'dem': 261,
+                'dem_pct': 30,
+                'rep': 174,
+                'rep_pct': 40
+            }
+
+*/
