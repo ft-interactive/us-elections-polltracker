f266ea16bba5a8eff95eedbb776385c24d46f36c
tomgp <tom.g.pearson@gmail.com>
2016-10-13 12:06:47 +0100

    add overview route for data

--- server/lib/getResultData.js
+++ server/lib/getResultData.js
@@ -10,18 +10,29 @@ export default async function getResult() {
                 .reduce((previous, current) => {
                         let r = previous.r;
                         let d = previous.d;
+                        let guess_r = previous.guess_r;
+                        let guess_d = previous.guess_d;
                         let g = previous.g;
                         let l = previous.l;
                         let reported = previous.reported;
 
-                        if(current.winner == 'r'){ r += current.ecvotes; }
-                        if(current.winner == 'd'){ d += current.ecvotes; }
-                        if(current.winner == 'g'){ g += current.ecvotes; }
-                        if(current.winner == 'l'){ l += current.ecvotes; }
+                        if(current.winner == 'r'){ 
+                            r += current.ecvotes; 
+                            guess_r += current.ecvotes;
+                        }else if(current.winner == 'd'){ 
+                            d += current.ecvotes; 
+                            guess_d += current.ecvotes;
+                        }else if(current.winner == 'g'){ g += current.ecvotes; }
+                        else if(current.winner == 'l'){ l += current.ecvotes; }
+                        else if(current.leaning == 'r'){ guess_r += current.ecvotes; }
+                        else if(current.leaning == 'd'){ guess_d += current.ecvotes; }
+
                         if(current.winner){ reported[current.code.substring(0,2)] = true; }
 
-                        return { r, d, g, l, total:previous.total, reported:reported};
-                    }, { r: 0, d: 0, g: 0, l:0, total: 538, reported:{} });
+                        return { r, d, g, l, guess_d:guess_d, guess_r:guess_r, total:previous.total, reported:reported};
+                    }, { r: 0, d: 0, g: 0, l:0, guess_d:0,guess_r:0, total: 538, reported:{} });
+
+console.log(totals);
 
             processed.pollClosingTimes = response.data.events;
             processed.senate = response.data.senate[0];
@@ -29,21 +40,20 @@ export default async function getResult() {
             processed.electoralCollege = response.data.electoralCollege;
             processed.ecTotals = totals;
             processed.overview = {
-                time: new Date(),
+                timestamp: (new Date()).getTime(),
                 senate: response.data.senate[0].current,
                 house: response.data.house[0].current,
                 president: {
                     clinton: totals.d,
                     trump: totals.r,
-                    clinton_pct: 100/totals.total * totals.d,
-                    trump_pct: 100/totals.total * totals.r,
-                    /*
-                    bestGuessClinton: 245,
-                    bestGuessTrump: 212,
-                    bestGuessClinton_pct: 45.5,
-                    bestGuessTrump_pct: 39.4, */
-                    states_reporting:Object.keys(totals.reported).length,
-                    isFinal: false
+                    clinton_pct: 100/ totals.total * totals.d,
+                    trump_pct: 100/ totals.total * totals.r,
+                    bestGuessClinton: totals.guess_d,
+                    bestGuessTrump: totals.guess_r,
+                    bestGuessClinton_pct: 100/ totals.total * totals.guess_d,
+                    bestGuessTrump_pct: 100/ totals.total * totals.guess_r,
+                    states_reporting: Object.keys(totals.reported).length,
+                    isFinal: ( (totals.r + totals.d) === totals.total ),
                 },
             }
 
