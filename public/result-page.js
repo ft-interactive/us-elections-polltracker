201fb2e932c59c471ce98ff401229c01667eb3be
tomgp <tom.g.pearson@gmail.com>
2016-10-18 13:59:41 +0100

    set up data binding for maps

--- public/result-page.js
+++ public/result-page.js
@@ -1,15 +1,53 @@
 //set polling frequency
 function getData() {
-  console.log('GET DATE');
-    d3.json('full-result.json',function(d){
-        if(d.overview.timestamp>timestamp){
-            timestamp = d.overview.timestamp;
-            console.log('ts');
+    d3.json('full-result.json',function(data) {
+        if(data.overview.timestamp > pageDataTimestamp){
+            pageDataTimestamp = data.overview.timestamp;
+            console.log('UPDATE');
+            rebind(data);
+            colourMap();
         }
-        console.log(timestamp);console.log(d);
     });
 }
 
+function rebind(data) {
+    var lookupByCollegeID = makeLookup(data.electoralCollege, 'code');
+    d3.selectAll('path.map-state')
+        .each(function(datum){      
+            var collegeID = d3.select(this).attr('id');
+            d3.select(this).datum( lookupByCollegeID[collegeID.toLowerCase()] );
+        });
+
+    d3.selectAll('circle.college-vote')
+        .each(function(datum){      
+            var collegeID = d3.select(this).attr('id').split('_')[0];
+            d3.select(this).datum( lookupByCollegeID[collegeID.toLowerCase()] );
+        });
+}
+
+function colourMap(){
+    d3.selectAll('path.map-state').transition().style('fill',function(d){
+        return '#000';
+    })
+    d3.selectAll('circle.college-vote').transition().style('fill',function(d){
+        return '#000';
+    })
+}
+
+function makeLookup(arr,key){
+    var o = {};
+    arr.forEach(function(d){
+        o[d[key]] = d;
+    })
+    return o;
+}
+
 queue('d3.v4.min.js', function() {
-  getData();
+    var pollingInterval = 3000;
+    window.setTimeout(function(){ 
+        window.setInterval(function(){
+            console.log('polling');
+            getData();
+        }, pollingInterval); 
+    }, pollingInterval);
 })
