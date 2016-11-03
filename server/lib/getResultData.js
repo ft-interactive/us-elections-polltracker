3ae2da2bd06e03602ff3417a0d3d879e294281bf
tomgp <tom.g.pearson@gmail.com>
2016-11-03 07:38:08 +0000

    change bertha results url
    switch to use the congress sheet

--- server/lib/getResultData.js
+++ server/lib/getResultData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import cache from '../lib/cache';
 
 // TODO: hit the republish endpoint on a CRON job or something
-const resultURL = 'http://bertha.ig.ft.com/view/publish/gss/17Ea2kjME9yqEUZfQHlPZNc6cqraBUGrxtuHj-ch5Lp4/copy,electoralCollege,senate,house,media';
+const resultURL = 'http://bertha.ig.ft.com/view/publish/gss/17Ea2kjME9yqEUZfQHlPZNc6cqraBUGrxtuHj-ch5Lp4/copy,electoralCollege,congress,media';
 
 export default function getResult() {
   return cache(
