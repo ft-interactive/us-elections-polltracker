5a48b663edaaf179c942b28d8c52039312ead846
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-11-04 09:41:19 +0000

    Return of the Link headers
    Add the Polyfill service to the Link header for Polls page.
    
    Also add all preload Links to the results page.

--- server/controllers/result.js
+++ server/controllers/result.js
@@ -6,8 +6,19 @@ import { getResultData } from '../lib/results';
 const maxAge = 10;
 const sMaxAge = 5;
 
+// IMPORTANT: this experiment hardcodes the polyfill service URL.
+// If we change the URL by altering its feature or flags then we also need to update it here too.
+const linkHeader = [
+                    '<g-ui/critical.css>; as="style"; rel="preload"; nopush',
+                    '<g-ui/main.css>; as="style"; rel="preload"; nopush',
+                    '<main.css>; as="style"; rel="preload"; nopush',
+                    '<g-ui/top.js>; as="script"; rel="preload"; nopush',
+                    '<https://cdn.polyfill.io/v2/polyfill.min.js?callback=igPolyfillsLoaded&features=default-3.6,matchMedia,fetch,IntersectionObserver,HTMLPictureElement,Map|always|gated,Array.from|always|gated,Array.prototype.includes|always|gated&flags=gated&unknown=polyfill>; as="script"; rel="preload"; nopush'
+                  ].join(', ');
+
 export async function page(req, res) {
   res.setHeader('Cache-Control', `public, max-age=30, s-maxage=10`);
+  res.setHeader('Link', linkHeader);
   try {
     const html = await cache(
       'result-html',
