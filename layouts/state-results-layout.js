0e66ac59e9062f035c8a50fe2a867981eb051dce
Callum Locke <callum.locke@gmail.com>
2016-11-05 16:13:31 -0400

    more styling; use shortnames in desktop layout; sort states by voting power

--- layouts/state-results-layout.js
+++ layouts/state-results-layout.js
@@ -1,5 +1,45 @@
 import color from './color';
-import states from '../data/states.json';
+import statesData from '../data/states.json';
+
+// make a quick lookup for objects in the statesData (from the JSON file in this
+// app) with keys based on the results spreadsheet
+const statesLookup = {};
+for (const obj of statesData) {
+  switch (obj.code) {
+    case 'MECD1':
+      statesLookup.me1 = obj;
+      break;
+    case 'MECD':
+      statesLookup.me2 = obj;
+      break;
+    case 'NECD1':
+      statesLookup.ne1 = obj;
+      break;
+    case 'NECD2':
+      statesLookup.ne2 = obj;
+      break;
+    case 'NECD3':
+      statesLookup.ne3 = obj;
+      break;
+    default:
+      statesLookup[obj.code.toLowerCase()] = obj;
+  }
+}
+
+// override certain shortnames to fit our table better
+const customShortNames = {
+  IL: 'Illinois',
+  NH: 'NH',
+  NC: 'NC',
+  SC: 'SC',
+  WV: 'WV',
+  RI: 'RI',
+};
+
+const getShortName = (code) => {
+  const data = statesLookup[code.toLowerCase()];
+  return customShortNames[code] || data.shortName || data.name;
+};
 
 // returns an appropriate [bgColor, textColor] for a given winner
 const getColors = (winner) => {
@@ -33,7 +73,7 @@ export default (electoralCollege) => {
         const [, code, districtNumber] = /([A-Za-z]+)([0-9]*)/.exec(state.code.toUpperCase());
         const winner = state.winner ? state.winner.toLowerCase() : null;
 
-        const shortName = code; // TODO
+        const shortName = getShortName(code);
 
         const ecVotesWidth = (ecVotes / 55) * 100; // top number of EC votes is 55 (Calif.)
 
@@ -48,6 +88,7 @@ export default (electoralCollege) => {
           textColor,
         };
       })
+      .sort((a, b) => b.ecVotes - a.ecVotes)
     ;
   }
 
