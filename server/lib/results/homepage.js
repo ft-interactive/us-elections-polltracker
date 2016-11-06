ae41187f451e084e56847e0b1c907781521f7ec2
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-11-06 21:57:00 +0000

    Remove mock homepage data

--- server/lib/results/homepage.js
+++ server/lib/results/homepage.js
@@ -1,42 +1,13 @@
 import fs from 'fs';
 import color from '../../../layouts/color';
 
-// TODO: use bertha sheet for all config
 // TODO: stop doing any results stuff if
 // TODO: what to do with "isFinal"? how does it affect the front end
 // TODO: Automatically write the footnote or substitute variables
-//         null means no message, [AUTO] means write is automatically, or substitute tokens [CLINTON], [NUM_CALL], [TIME]
 // TODO: how to print the time in the footnote for all locales. safe HTML with o-date
 // TODO: decide sensible defaults for everything
 // TODO: validate all values coming from Bertha config
-function makeHomepageData() {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(__dirname + '/mockhomepage.json', 'utf8', function(err, str) {
-      if (err) {
-        reject(err);
-        return;
-      }
 
-      var data = JSON.parse(str);
-
-      data.updated = Date.now();
-
-      const trump = 'red';
-      const clinton = 'blue';
-
-      data.resultsPromo.stateFills = {
-        NY: clinton,
-        nj: clinton,
-        TX: trump,
-        fl: trump,
-        OH: trump,
-        FOO: trump,
-        BAR: clinton,
-      }
-      resolve(data);
-    });
-  });
-}
 
 // If a tab doesn't appear in this list then it's an unknown tab name
 // If a tab does appear in this list but it's value is false
