c66e871eb81f762dabe06c9866e2a64bebf69bc7
Callum Locke <callum.locke@gmail.com>
2016-11-07 15:16:41 -0500

    minify assets (#337)

--- tripfile.js
+++ tripfile.js
@@ -1,5 +1,6 @@
 import Directory from 'exhibit-directory';
 import { compose, plugin } from 'exhibit';
+import cssnano from 'cssnano';
 
 const client = new Directory('client', { log: true });
 const dist = new Directory('dist', { log: true });
@@ -11,8 +12,17 @@ const transform = compose(
   plugin('browserify', { root: 'client' }),
 );
 
+const minify = compose(
+  plugin('uglify'),
+  plugin('postcss', cssnano(), { map: false }),
+);
+
 export const build = async () => {
-  await client.read().then(transform).then(dist.write);
+  await client.read().then(compose(
+    transform,
+    minify,
+    dist.write
+  ));
 };
 
 export const watch = async () => {
