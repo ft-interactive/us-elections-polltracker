34037bbe3ca433bc6e06ee2a457851237dcd758b
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-11-07 23:05:28 +0000

    Moar cache

--- server/lib/cache.js
+++ server/lib/cache.js
@@ -7,7 +7,7 @@ const maxAge = 60 * 1000; // 60 secs
 
 const lru = lruCache({
   maxAge,
-  max: 2400000,
+  max: 2600000,
   stale: true,
   length: (value, key) =>
       (!value ? 0 : JSON.stringify(value).length) + JSON.stringify(key).length
