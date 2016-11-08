9bd21c6dac80e968bf1bce78ea8425d86a357ffc
tomgp <tom.g.pearson@gmail.com>
2016-11-08 08:59:46 +0000

    update social sharing image

--- server/pages/createResultPage.js
+++ server/pages/createResultPage.js
@@ -14,7 +14,7 @@ class ResultPage extends Page {
 
   canonicalUrl = 'https://ig.ft.com/us-elections/results';
   mainImage = {
-      url: 'https://www.ft.com/__origami/service/image/v2/images/raw/https:%2F%2Fig.ft.com%2Fstatic%2Fus-election-2016%2Fsocial.jpg?source=ig&format=auto&quality=high&width=800',
+      url: 'https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fim.ft-static.com%2Fcontent%2Fimages%2F648a78f0-a510-11e6-8b69-02899e8bd9d1.img?url=http%253A%252F%252Fim.ft-static.com%252Fcontent%252Fimages%252F648a78f0-a510-11e6-8b69-02899e8bd9d1.img&source=ig&width=600&height=338&fit=cover&format=auto&quality=high',
   };
 
   id = '5cc27b78-946b-11e6-a1dc-bdf38d484582';
