2e83e97289081cec9d1e96b18a3b9cea0a06e9f2
Joanna Kao <joanna.kao@ft.com>
2016-11-09 18:48:20 -0500

    oops. bad logic.

--- server/lib/state-counts.js
+++ server/lib/state-counts.js
@@ -22,7 +22,7 @@ import db from '../../models';
 // runs a psql query to get the latest polling averages
 // for all states (choose 3 or 4-way based on displayRace)
 const latestAveragesByState = (pollnumcandidates) => {
-  if (!pollnumcandidates <= 5) throw new Error('Invalid pollnumcandidates');
+  if (pollnumcandidates > 5) throw new Error('Invalid pollnumcandidates');
 
   return db.sequelize.query(
     `SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY state ORDER BY date DESC) AS r, t.* FROM (SELECT * FROM "Pollaverages" WHERE pollnumcandidates = ${pollnumcandidates}) t) x WHERE x.r <= ${pollnumcandidates};`,
